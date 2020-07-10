fb17d0c21975556e19818193ec54f5ad1cd0e1ac
jerome <jeromerg@gmx.net>
2020-07-10 13:35:42 +0200

    missing dependencies in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="filoc",
-    version="0.0.6",
+    version="0.0.7",
     author="jeromerg",
     author_email="jeromerg@gmx.net",
     description="eases the saving and reading of files within a structured folder tree",
@@ -21,6 +21,8 @@ setuptools.setup(
     install_requires=[
         'fsspec',
         'parse',
+        'frozendict',
+        'orderedset',
     ],
     python_requires='>=3',
 )
