7ef30c53317afba4f8bfbe72092271828d8374d2
jeromerg <jeromerg>
2020-10-29 23:09:40 +0100

    added example_covid_github.html file

--- examples/build_examples.sh
+++ examples/build_examples.sh
@@ -1,2 +1,3 @@
 jupyter nbconvert example_ml.ipynb --TagRemovePreprocessor.remove_cell_tags='{"hide_cell"}'
+jupyter nbconvert example_covid_github.ipynb --TagRemovePreprocessor.remove_cell_tags='{"hide_cell"}'
 
